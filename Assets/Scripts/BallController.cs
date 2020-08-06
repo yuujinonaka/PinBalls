80d4d67220e5830ebc122204789358ba83a641b5
yuujinonaka <conect1208@gmail.com>
2020-08-06 17:08:41 +0900

    レッスン５の課題
    お試しで送ります。

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosz = -6.5f;
+
+    //ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //シーンの中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosz)
+        {
+            //GameoverTextにゲーム―オーバを表示
+            this.gameoverText.GetComponent<Text>().text = " GameOver";
+        }
+		
+	}
+}
---
+++ Assets/Scripts/ScoreManger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManger:MonoBehaviour
+{
+    public GameObject score_object = null;//Textオブジェクト
+    public int score = 0;//スコア変数
+
+    //初期化
+    void Start()
+    {
+        score = 0;
+        
+    }
+
+    //ボールと衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "SmallCloudTag")
+        {
+            score += 50;
+        }
+        else if (yourTag == "LargeCloudTag")
+        {
+            score += 100;
+        }
+        else if (yourTag == "LargeStarTag")
+        {
+            score += 200;
+        }
+
+        
+
+    }
+
+    // 更新
+    void Update()
+    {
+        //オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text>();
+        //テキストの表示を入れ替える
+        score_text.text = "Score:" + score;
+
+
+
+    }
+}
